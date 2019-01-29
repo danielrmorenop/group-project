de0560406549b34e6c94d12bba881a4e585021f3
Brenda Garcia <brenda.garcia007@mymdc.net>
2019-01-28 20:21:32 -0500

    produces images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var genres = [];
+var genres = ["https://m.media-amazon.com/images/M/MV5BMzg4MDJhMDMtYmJiMS00ZDZmLThmZWUtYTMwZDM1YTc5MWE2XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg","https://m.media-amazon.com/images/M/MV5BZjA3YjdhMWEtYjc2Ni00YzVlLWI0MTUtMGZmNTJjNmU0Yzk2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg","https://m.media-amazon.com/images/M/MV5BMTQ5MjI1OTA2NV5BMl5BanBnXkFtZTgwNjMxMjE3NjE@._V1_SX300.jpg"];
 
 //function to display the buttons on the page
 
@@ -6,10 +6,6 @@ $('#add-movie').on("click", function(event){
   event.preventDefault();
     //grab value of user input
   var userInput = $("#genreInput").val("");
-  genres.push(userInput);
-  genres.push(userInput);
-  genres.push(userInput);
-
 
   //loop through array
   for(var i=0; i<genres.length; i++){
@@ -17,7 +13,7 @@ $('#add-movie').on("click", function(event){
       movieDiv.addClass('card');
       movieDiv.css('width','18rem');
       var image = $('<img>');
-      image.attr('src', 'https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+      image.attr('src', genres[i]);
       movieDiv.append(image);
 
       $('#movies-appear-here').append(movieDiv);
