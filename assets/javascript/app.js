2ccbcd11ed87fcb7acf5112e618a0c4ba4879e41
“Daniel <daniel.moreno94@me.com>
2019-01-30 17:16:32 -0500

    random movie works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,46 @@
 // cache some shit
-	var api_key = "a97f71ed06e6f46f60b4fad70c2bd407";
+var api_key = "a97f71ed06e6f46f60b4fad70c2bd407";
 
-	//function definition
-	$('#add-movie').on("click", function(e){
- // this gets rid of the warning about using return false
-		e.preventDefault();
+//function definition
+$('#add-movie').on("click", function(e){
+// this gets rid of the warning about using return false
+	e.preventDefault();
 
-			var year = $('#genreInput').val();
-			var baseimg= "http://image.tmdb.org/t/p/w200//"
+		var year = $('#genreInput').val();
+		var baseimg= "http://image.tmdb.org/t/p/w200//"
 
-					$.getJSON("https://api.themoviedb.org/3/discover/movie?primary_release_year="+year+"&sort_by=vote_average.desc&api_key="+api_key,
-							function(json) {
-							console.log(json);
-							
-							for(i=0; i<4; i++){
-								
-							if (json.total_results) {
-								var produce = baseimg + json.results[i].poster_path
-								$('#movies-appear-here').append('<img src=' + produce + ' />')
-							}
-						}
+				$.getJSON("https://api.themoviedb.org/3/discover/movie?primary_release_year="+year+"&sort_by=vote_average.desc&api_key="+api_key,
+						function(json) {
+						console.log(json);
+						
+						var i = 0;
+
+							while( i < 4 ) {
+
+								var randNum = Math.floor( Math.random() * 19 );
+								var randomArr=[]
+
+								// ONLY if we are selecting a new random number...
+								if( randomArr.includes( randNum ) === false ) {
+
+									console.log( randNum );
+
+									// push number to our checker array
+									randomArr.push( randNum );
+									console.log(randomArr);
+
+									//make movie poster code
+									if (json.total_results) {
+										var produce = baseimg + json.results[randNum].poster_path
+										$('#movies-appear-here').append('<img src=' + produce + ' />')
+									/*----------------------------------------*/
+
+									// increase our counter
+									i++;
+
+								}
+							}}
 				})})
 
+				
 //==============================================================================================================================
